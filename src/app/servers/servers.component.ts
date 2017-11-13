5ee691e45498618c1fa722d9eaecff00448c39c1
DaanTacoSchool <djmessen@avans.nl>
2017-11-13 17:11:49 +0100

    section 2 own

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,15 +4,36 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 //  selector: '[app-servers]',
 //  selector: '.app-servers',
   selector: 'app-servers',
-  template: `<app-server></app-server><app-server></app-server>`,
+  // template: `<app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
   encapsulation: ViewEncapsulation.None
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'no server was created';
+  serverName= 'tstserver';
+  serverCreated=false;
+  servers = ['Testserver', 'Testeserver2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(()=>{
+      this.allowNewServer=true;
+    },2000);
+  }
 
   ngOnInit() {
   }
+
+  onCreateServer(){
+    this.serverCreated=true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus='server was created name is'+this.serverName;
+  }
+
+  onUpdateServerName(event: any){
+    this.serverName = event.target.value;
+  }
+
 
 }
